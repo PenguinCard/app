0d2d1542c6bc9ff1e4c4021e66bd5a748c36e602
김진수 <kimjinsu@gimjinsuui-MacBookAir.local>
2022-04-12 02:25:32 +0900

    loginform 구현 진행2

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,7 +4,41 @@ function LoginForm() {
     return (
         <div className="loginBackGround">
             <div className="loginFormStyle">
-
+                <div className="row" style={{ margin: "10px 0"}}>
+                    <div className="col">
+                        <h2 style={{ textAlign: "center"}}>LOGIN</h2>
+                    </div>
+                </div>
+                <div className="row" style={{ margin: "10px 0"}}>
+                    <div className="col">
+                        <div className="form-floating mb-3">
+                            <input type="email"
+                                   className="form-control"
+                                   id="floatingInput"
+                                   placeholder="name@example.com"
+                                   style={{ height: "60px"}}
+                            />
+                            <label htmlFor="floatingInput">Email address</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="row" style={{ margin: "10px 0"}}>
+                    <div className="col">
+                        <div className="form-floating mb-3">
+                            <input type="password"
+                                   className="form-control"
+                                   id="floatingPassword"
+                                   placeholder="Password"
+                                   style={{ height: "60px"}}/>
+                                <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="row" style={{ margin: "10px 0"}}>
+                    <div className="col">
+                        <button type="button" className="btn btn-dark mb-3" style={{ width: "100%", height: "60px" }}>LOGIN</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
